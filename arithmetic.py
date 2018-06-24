e61f8185d7042a131ea208601afe052ee3ff8f96
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-24 12:28:27 -0700

    finished power function

--- arithmetic.py
+++ arithmetic.py
@@ -8,14 +8,11 @@ def add(num1, num2):
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-
     return num1 - num2
-
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
     return num1 * num2
-
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
@@ -31,7 +28,7 @@ def cube(num1):
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-
+    return num1 ** num2
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
